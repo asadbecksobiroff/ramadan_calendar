8c5e1a44b5917d997b643b9102cc7d64d50dace5
asadbecksobiroff <108651766+asadbecksobiroff@users.noreply.github.com>
2024-03-15 20:36:53 +0500

    commit functions.py
    bir xil so'rov yuborilganda request jo'natish oldi olindi

--- functions.py
+++ functions.py
@@ -1,12 +1,89 @@
 from bs4 import BeautifulSoup
 import requests
-from datetime import datetime
-
-def get_time():
-    return datetime.now()
+from datetime import datetime, timedelta
 
 
-def get_times(code):
+data = {
+    1: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    4: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    5: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    9: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    14: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    15: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    16: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    18: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    25: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    27: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    37: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    39: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    74: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+    78: {
+        'date': None,
+        'saharlik': None,
+        'iftorlik': None
+    },
+}
+
+
+def get_date():
+    today = datetime.now()
+    todayGMT5 = today + timedelta(hours=5)
+    return todayGMT5
+
+
+def get_times(city):
     locations = {
         'andijon': 1,
         'fergana': 37,
@@ -23,17 +100,16 @@ def get_times(code):
         'toshkent': 27,
         'rishton': 39,
     }
+        
     
-    # proxiesDict = {
-    #               "http": "http://10.10.1.10:3128",
-    #               "https": "https://10.10.1.10:1080",
-    #             }
-    # PROXY_URL = "http://proxy.server:3128"
+    today = get_date()
+    city_code = locations[city]
     
-    month = datetime.now().month
-    city = locations[code]
-    url = f'https://islom.uz/vaqtlar/{city}/{month}'
-    # r = requests.get(url, proxies=proxiesDict)
+    if data[city_code]['date'] == today.date():
+        return data[city_code]['saharlik'], data[city_code]['iftorlik']
+    
+    month = today.month
+    url = f'https://islom.uz/vaqtlar/{city_code}/{month}'
     r = requests.get(url)
     
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -41,5 +117,8 @@ def get_times(code):
     saharlik = times[0].text
     iftorlik = times[1].text
     
+    data[city_code]['date'] = today.date()
+    data[city_code]['saharlik'] = saharlik
+    data[city_code]['iftorlik'] = iftorlik
+        
     return saharlik, iftorlik
-
